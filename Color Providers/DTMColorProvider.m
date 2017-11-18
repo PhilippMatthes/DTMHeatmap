c20c1ffe8259ac4360d6daaf902c78e75f11691c
Philipp Matthes <github@philippmatth.es>
2017-11-18 14:08:36 +0100

    Support for custom colors

--- Color Providers/DTMColorProvider.m
+++ Color Providers/DTMColorProvider.m
@@ -10,6 +10,17 @@
 
 @implementation DTMColorProvider
 
+static CGFloat maxRed = 1.0;
+static CGFloat maxGreen = 1.0;
+static CGFloat maxBlue = 1.0;
+
+- (void)red:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue
+{
+    maxRed = red;
+    maxGreen = green;
+    maxBlue = blue;
+}
+
 - (void)colorForValue:(double)value
                   red:(CGFloat *)red
                 green:(CGFloat *)green
@@ -29,34 +40,16 @@
         *alpha = kSBAlphaPivotY + ((kSBMaxAlpha - kSBAlphaPivotY) / (1 - kSBAlphaPivotX)) * (value - kSBAlphaPivotX);
     }
     
+    
+    
     //formula converts a number from 0 to 1.0 to an rgb color.
     //uses MATLAB/Octave colorbar code
-    if (value <= 0) {
-        *red = *green = *blue = *alpha = 0;
-    } else if (value < 0.125) {
-        *red = *green = 0;
-        *blue = 4 * (value + 0.125);
-    } else if (value < 0.375) {
-        *red = 0;
-        *green = 4 * (value - 0.125);
-        *blue = 1;
-    } else if (value < 0.625) {
-        *red = 4 * (value - 0.375);
-        *green = 1;
-        *blue = 1 - 4 * (value - 0.375);
-    } else if (value < 0.875) {
-        *red = 1;
-        *green = 1 - 4 * (value - 0.625);
-        *blue = 0;
-    } else {
-        *red = MAX(1 - 4 * (value - 0.875), 0.5);
-        *green = *blue = 0;
-    }
+    
     
     *alpha *= maxVal;
-    *blue *= *alpha;
-    *green *= *alpha;
-    *red *= *alpha;
+    *red = value * maxRed;
+    *green = value * maxGreen;
+    *blue = value * maxBlue;
 }
 
 @end
